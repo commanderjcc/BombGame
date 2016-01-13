7983f1fa2a5b548d9e89fa7d4a62680d463dfa3a
Josh C <legomania@cox.net>
2016-01-13 06:10:15 -0800

    Finished!
    added 2 buttons 2bombs and hooked up to hide/show when pressed

--- BombGame/ViewController.swift
+++ BombGame/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var blueBomb: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func redButton(sender: AnyObject) {
+        redBomb.hidden = true
+        blueBomb.hidden = false
+    }
+    
+    @IBAction func blueButton(sender: AnyObject) {
+        blueBomb.hidden = true
+        redBomb.hidden = false
+    }
+    
 }
 
